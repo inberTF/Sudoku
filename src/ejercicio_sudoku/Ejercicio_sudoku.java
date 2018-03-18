41c54df441507aba0a46a485e03f5366fd0adadf
ivan <ivan@ivan-HP-Compaq-6720s>
2018-03-18 19:22:24 +0100

    El programa acabado, te permite jugar hasta resolver el sudoku

--- src/ejercicio_sudoku/Ejercicio_sudoku.java
+++ src/ejercicio_sudoku/Ejercicio_sudoku.java
@@ -5,6 +5,7 @@
  */
 package ejercicio_sudoku;
 
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,11 +19,62 @@ public class Ejercicio_sudoku {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        
+
         Sudoku su = new Sudoku();
-        su.inicializar();
-        System.out.println(su.toString());
+        int fila, columna, elemento;
+        boolean con = false;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("SUDOKU:\n"
+                + "1- JUGAR\n"
+                + "2- SALIR");
+        int op = teclado.nextInt();
+
+        if (op == 1) {
+            su.inicializar();
+            while (con == false) {
+                System.out.println("¿Que Quieres Hacer\n?"
+                        + "1- añadir un numero\n"
+                        + "2- ver sudoku");
+                op = teclado.nextInt();
+                switch (op) {
+                    case 1:
+                        System.out.println("Dime la fila:");
+                        fila = teclado.nextInt();
+                        System.out.println("Dime la columna:");
+                        columna = teclado.nextInt();
+                        System.out.println("Dime el numero que quieres añadir:");
+                        elemento = teclado.nextInt();
+                         {
+                            try {
+                                su.modificarElemento(fila, columna, elemento);
+                            } catch (SudokuException ex) {
+                                Logger.getLogger(Ejercicio_sudoku.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                        }
+                        break;
+                    case 2:
+                        System.out.println(su.toString());
+                        break;
+
+                }
+                boolean con2= false;
+                for (int i = 0; i < su.fila.size(); i++) {
+                    for (int j = 0; j < su.fila.get(i).size(); j++) {
+                        
+                        if (su.fila.get(i).get(j) == 0) {
+                            con = false;
+                            con2 = true;
+                        }
+                        if((su.fila.get(i).get(j) !=0) && (con2 = false))
+                        {
+                            System.out.println("Has completado el sudoku");
+                            con = true;
+                        }
+                        
+                    }
+                }
+            }
+        }
     }
-    
+
 }
